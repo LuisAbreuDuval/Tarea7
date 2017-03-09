f0d1f93b15f1a9315b7b6e19ddfd207a6c0debcb
LuisAbreuDuval <el-luis.29@outlook.com>
2017-03-09 14:02:19 -0400

    Semaforo

---
+++ Semaforo/confi.php
@@ -0,0 +1,25 @@
+<?php
+
+if($_POST){
+  file_put_contents('confi.txt', json_encode($_POST));
+}
+  $datos = json_decode (file_get_contents('confi.txt'),1);
+
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+   <head>
+     <meta charset="utf-8">
+     <title> Configuracion </title>
+  </head>
+  <body>
+<form method="post" action="">
+Tiempo en verde: <input value="<?php echo $datos['verde']; ?>" type="text" name="verde" />
+Tiempo en rojo: <input value="<?php echo $datos['rojo']; ?>" type="text" name="rojo" />
+  <button type="submit"> Guardar </button>
+</form>
+  </body>
+
+  </html>
